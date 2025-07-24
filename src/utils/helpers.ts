a9ffc9f22a59d662ce807cb13574a58ee7d36ef6
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-07-24 15:11:31 +0200

    refactor: move cn function to a new file for better organization

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,10 +1,3 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export const cn = (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs));
-};
-
 export const formatDate = (date: Date): string => {
   return date.toLocaleDateString();
 };
