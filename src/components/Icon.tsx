e51135af81adc455398c34353d172fb20e4e0e46
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-07-30 10:34:49 +0200

    feat: add onClick and onClose props to Label and Icon components

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,17 +1,19 @@
-import { cn } from "@/utils";
+import { cn } from '@/utils';
 
 interface IconProps {
   name: string;
   size?: number;
   className?: string;
+  onClick?: () => void;
 }
 
-const Icon = ({ name, size = 22, className }: IconProps) => (
+const Icon = ({ name, size = 22, className, onClick }: IconProps) => (
   <span
     className={cn('material-symbols-outlined text-primary-dark', className)}
     style={{
       fontSize: `${size}px`,
     }}
+    onClick={onClick}
   >
     {name}
   </span>
--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -2,15 +2,21 @@ import Icon from '@/components/Icon';
 
 interface LabelProps {
   label: string;
+  onClick?: () => void;
+  onClose?: () => void;
 }
 
-const Label = ({ label }: LabelProps) => (
-  <div className="group relative min-w-12 rounded-full border px-2.5 py-0.5 text-[11px]">
+const Label = ({ label, onClick, onClose }: LabelProps) => (
+  <div
+    className="group relative min-w-12 cursor-pointer rounded-full border px-2.5 py-0.5 text-[11px]"
+    onClick={onClick}
+  >
     <div className="truncate group-hover:w-4/5">{label}</div>
     <Icon
       name="close"
       size={11}
-      className="absolute top-[7px] right-1.5 opacity-0 transition-opacity group-hover:opacity-100"
+      className="hover:bg-base-light absolute top-[4px] right-[2px] rounded-full p-[2px] opacity-0 group-hover:opacity-100"
+      onClick={onClose}
     />
   </div>
 );
