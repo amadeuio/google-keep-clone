9d9b7f698ab8a4595d7f480257e7f38bc60f2c71
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-11-08 12:29:33 -0600

    feat: center notes grid

--- src/components/layout/Main/Main.tsx
+++ src/components/layout/Main/Main.tsx
@@ -1,25 +1,28 @@
 import { NoteCreate, NoteView } from '@/components';
 import { useResponsiveGrid } from '@/hooks';
 import { useStore } from '@/store';
-import { selectDisplayNotes } from '@/store/selectors';
+import { selectDisplayNotes, selectNotesTotalWidth } from '@/store/selectors';
 import { useRef } from 'react';
 import EmptyState from './EmptyState';
 
 const Main = () => {
   const notes = useStore(selectDisplayNotes);
+  const notesTotalWidth = useStore(selectNotesTotalWidth);
   const containerRef = useRef<HTMLDivElement>(null);
   useResponsiveGrid(containerRef);
 
   return (
-    <main className="flex flex-1 flex-col items-center gap-20 overflow-y-auto p-4 py-8">
+    <main className="flex flex-1 flex-col items-center gap-20 overflow-y-auto px-2 py-12">
       <NoteCreate />
       {notes.length === 0 ? (
         <EmptyState />
       ) : (
-        <div ref={containerRef} className="relative w-full">
-          {notes.map((note) => (
-            <NoteView key={note.id} note={note} />
-          ))}
+        <div ref={containerRef} className="w-full">
+          <div className="relative mx-auto" style={{ width: notesTotalWidth }}>
+            {notes.map((note) => (
+              <NoteView key={note.id} note={note} />
+            ))}
+          </div>
         </div>
       )}
     </main>
