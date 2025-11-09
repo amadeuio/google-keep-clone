4a68724e297828d21c9ed62d748f47e1219c50b4
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-11-09 14:45:25 -0600

    refactor: memoize useClickOutside callback with a ref to avoid calling the effect on overy caller render

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -2,18 +2,22 @@ import { useEffect, useRef } from 'react';
 
 export const useClickOutside = (onClickOutside: () => void) => {
   const triggerRef = useRef<HTMLDivElement>(null);
+  const callbackRef = useRef(onClickOutside);
+
+  useEffect(() => {
+    callbackRef.current = onClickOutside;
+  }, [onClickOutside]);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (triggerRef.current && !triggerRef.current.contains(event.target as Node)) {
-        onClickOutside?.();
+        callbackRef.current();
       }
     };
 
     document.addEventListener('mousedown', handleClickOutside);
-
     return () => document.removeEventListener('mousedown', handleClickOutside);
-  }, [onClickOutside]);
+  }, []);
 
   return { triggerRef };
 };
