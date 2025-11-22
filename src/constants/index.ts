0844868d8fb9abdeedf679df9a78e2a67fc92dc8
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-11-22 12:05:32 -0600

    refactor: update constants index to explicit exports for consistency

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-export * from './colors';
-export * from './grid';
-export * from './notes';
+export { COLORS } from './colors';
+export { GRID_GAP, PINNED_UNPINNED_GAP } from './grid';
+export { NOTE_WIDTH, NOTE_WIDTH_EXPANDED } from './notes';
